389f98a30effd96d8805fa0e78805b438de26e9a
juliaacarvalho <146783405+juliaacarvalho@users.noreply.github.com>
2023-10-27 17:26:15 -0300

    Add files via upload
    Repositório atualizado

---
+++ Julia/js/script.copy.js
@@ -0,0 +1,27 @@
+// 
+
+
+
+
+
+
+
+
+
+/* "const" = Escopo Global - boa pratica é "declar" variavel
+const album = document.getElementById('albumFotos');  
+const site = document.getElementById('site');
+const port = document.getElementById('port');
+
+const movieAlbum = document.querySelector('.movieAlbum');
+const moviesite = document.querySelector('.moviesite');
+const movieport = document.querySelector('.movieport');
+
+
+// evento de click
+album.addEventListener("click",() => {
+    album.classList.toggle('active');
+    movieAlbum.classList.toggle('active');
+});
+
+*/
